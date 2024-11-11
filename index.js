017e579cfd8a92a6ed0afa3a1c13bf46979478ec
vibhushit <vibhushit10@gmail.com>
2024-11-12 00:41:25 +0530

    add: wallet and balance-check

--- index.js
+++ index.js
@@ -11,5 +11,18 @@ const {
  const publicKey = new PublicKey(wallet._keypair.publicKey)
  const secretKey = wallet._keypair.secretKey
 
- console.log(publicKey)
- console.log(secretKey)
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
